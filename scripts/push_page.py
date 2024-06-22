05a1e19646a83851942669983a0a8ba01e694b23
chenbinbin <chenbinbin20@huawei.com>
2024-06-22 22:22:22 +0800

    增加脚本

--- scripts/push_page.py
+++ scripts/push_page.py
@@ -1,22 +1,28 @@
 import subprocess
+import os
 
-
-#1.进入到上一级目录massive
-subprocess.run('cd ..', shell=True)
+#1.将当前目录切换到上一层目录
+os.chdir('..')
 
 #2.将分支切换到master
+print("1.change branch to master.....")
 subprocess.run('git checkout master', shell=True)
 
 #3.更新代码git pull
+print("2.git pull.....")
 subprocess.run('git pull', shell=True)
 
 #4.gitbook build
+print("3.gitbook build.....")
 subprocess.run('gitbook build', shell=True)
 
 #5.将_book目录的内容拷贝book目录
-subprocess.run('cp -r ./book ../', shell=True)
+print("4.copy _book.....")
+subprocess.run('cp -r ./_book ../', shell=True)
 
 #6.git切换到gh-pages
+print("5.git checkout gh-pages.....")
+subprocess.run('git checkout gh-pages', shell=True)
 
 #6.将当前的内容都删除，除了.git目录
 
